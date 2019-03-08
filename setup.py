26e31dd881188a0568ca916478fe564757c562a1
Erik Parreira <etinlb@gmail.com>
2019-03-08 16:50:51 -0500

    Using setup tools find package

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import sys
 
 
@@ -9,6 +9,7 @@ setup(name="quake3_log_parser",
       version='0.1',
       python_requires='>3.4.0',
       author="ET",
+      packages=find_packages(),
       entry_points="""
       [console_scripts]
       quake3-log-parser=quake3_log_parser.parser:main
